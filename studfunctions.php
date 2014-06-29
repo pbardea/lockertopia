e4d225b855e7cae046aaf2bae597f52b4a2226ea
pbardea <pbardea@gmail.com>
2014-06-29 17:55:11 -0400

    Generalize studfun

--- studfunctions.php
+++ studfunctions.php
@@ -1,11 +1,11 @@
 <?php
 function openConnect(){//Opens a MySQl connection
-	$connect = mysql_connect("localhost", "root", "root") or die(mysql_error()); 
+	$connect = mysql_connect("localhost", "db_userName", "db_passWord") or die(mysql_error()); 
 	mysql_select_db("lockertopia") or die(mysql_error()); //creates a MySQL connection
 }
 
 function closeConnect(){//Closes MySQL connection
-	mysql_close(mysql_connect("localhost", "root", "root"));//Closes MySQL connection
+	mysql_close(mysql_connect("localhost", "db_userName", "db_passWord"));//Closes MySQL connection
 }
 
 ?>
